10fca6ed9f228574cc7a66ec7204650e9151e239
Cino Playz <90247617+CinoPlayz@users.noreply.github.com>
2021-12-30 00:03:03 +0100

    Ustvarjanje API končano

--- api/ustvarjanje.php
+++ api/ustvarjanje.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   
-
+//Dobi podatke o Authorization, ki so v headerju
 if (isset($_SERVER['Authorization'])) {
     $headers = trim($_SERVER["Authorization"]);
 }
@@ -20,7 +20,7 @@ elseif (function_exists('apache_request_headers')) { //Apache, ter drugi
     }
 }
 
-
+//Če ni vrne, da nima dostopa
 if(empty($headers)){
     http_response_code(401);
     exit;
@@ -28,12 +28,13 @@ if(empty($headers)){
 
 require("../PovezavaZBazo.php");
 
+//Filtrira header, da nima kej z SQL ter XSS
 $headersfilterSQL = mysqli_real_escape_string($povezava, $headers);
 $headersfilter = htmlspecialchars($headersfilterSQL, ENT_QUOTES);
 
 $token = str_replace("Bearer ", "", $headersfilter);
 
-
+//Preveri, če sploh obstaja ta token v bazi
 $sql = "SELECT * FROM Uporabnik WHERE TokenWeb='". hash("sha256", $token) . "'";
 
 $rezultat = mysqli_query($povezava, $sql);
@@ -53,7 +54,14 @@ if(mysqli_num_rows($rezultat) > 0){
             $Stolpci = BranjeStolpcev($tabela, $povezava);
         }
         else{
-
+            if($tabela == "Uporabnik"){
+                mysqli_close($povezava);
+                http_response_code(403); 
+                exit;
+            }
+            else{
+                $Stolpci = BranjeStolpcev($tabela, $povezava);
+            }
         }
 
         $podatki = json_decode(file_get_contents("php://input"));
@@ -82,9 +90,36 @@ if(mysqli_num_rows($rezultat) > 0){
                     exit;
                 }
                 else{
-                    //Shrani podatke, če niso prazni, ter spremeni $podatkifilter tako da je empty
-                    array_push($StolpciZPodatki, array($stolpec, $podatkifilter, "NO"));
-                    $podatkifilter = "";
+                    //Najprej preveri če je Geslo, ter to nato Hasha in shrani podatke, če niso prazni, ter spremeni $podatkifilter tako da je empty. Zadnji del isto za else
+                    if($stolpec == "Geslo"){
+
+                        if(defined('PASSWORD_ARGON2ID')) {
+                            $geslohash = password_hash($podatkifilter, PASSWORD_ARGON2ID, ['memory_cost' => 2048, 'time_cost' => 12, 'threads' => 2]);
+                        }
+                        else{
+                            $geslohash = password_hash($podatkifilter, PASSWORD_DEFAULT, ['memory_cost' => 2048, 'time_cost' => 12, 'threads' => 2]);
+                        }
+
+                        array_push($StolpciZPodatki, array($stolpec, $geslohash, "NO"));
+                        $podatkifilter = "";
+                    }//Pregleda, da so pravila Admin oz Uporabnik, drugače ne izvede nič pozneje
+                    else if($stolpec == "Pravila"){
+                        if($podatkifilter != "Admin" && $podatkifilter != "Uporabnik"){
+                            http_response_code(400);
+                            echo json_encode(array("sporocilo" => "Pravilo ne obstaja"));
+                            exit;
+                        }
+
+                        array_push($StolpciZPodatki, array($stolpec, $podatkifilter, "NO"));
+                        $podatkifilter = "";
+
+                    }
+                    else{
+                        array_push($StolpciZPodatki, array($stolpec, $podatkifilter, "NO"));
+                        $podatkifilter = "";
+                    }
+
+                    
                 }
             }  
             else{
@@ -99,13 +134,78 @@ if(mysqli_num_rows($rezultat) > 0){
                   
             }          
         }
+
+        //SQL stavek razdljen v dva dela za vnos ter kako velik je array $StolpciZpodatki
+        $sqlPrviDel = "INSERT INTO $tabela(";
+        $sqlDrugiDel = ") VALUES (";
+
+        $kolikoPodatkov = count($StolpciZPodatki);
         
-        //print_r($podatki);
-        print_r($StolpciZPodatki);
+        //Gre čez vsak element v arrayu(V tem primeru je 2D)
+        for($i = 0; $i < $kolikoPodatkov; $i++){
+
+            //Preveri, da ne po slučajno vpisal podatkov v spodnja dva stolpca
+            if($StolpciZPodatki[$i][0] != "TokenWeb" && $StolpciZPodatki[$i][0] != "TokenAndroid"){
+                
+                //Dodaja stavek skupaj, če je zadenj vnos se ne bo vpisala "," v stavek
+                if($i == ($kolikoPodatkov-1)){
+                    $sqlPrviDel .= $StolpciZPodatki[$i][0];
+                }
+                else{
+                    $sqlPrviDel .= $StolpciZPodatki[$i][0]. ", ";
+                }
+                
+
+                //Prveri kateri element je če je string doda še '' v stavek drugače pusti prazno, za NULL doda samo NULL v stavek
+                if(is_numeric($StolpciZPodatki[$i][1])){
+                    $sqlDrugiDel .= $StolpciZPodatki[$i][1];
+                }
+                else if($StolpciZPodatki[$i][1] == "NULL"){
+                    $sqlDrugiDel .= "NULL";
+                }
+                else{
+                    $sqlDrugiDel .= "'" . $StolpciZPodatki[$i][1] . "'";
+                }
+
+                //Doda vejico, če ni zadenj vnos drugače samo zaključi stavek
+                if($i == ($kolikoPodatkov-1)){
+                    $sqlDrugiDel .= ");";
+                }
+                else{
+                    $sqlDrugiDel .= ", ";
+                }
+
+
+                
+            }
+
+        }
         
-        mysqli_close($povezava);
+        $sql = $sqlPrviDel . $sqlDrugiDel;
+
+        if(mysqli_query($povezava, $sql)){            
+            http_response_code(200);
+            exit;
+        }
+        else{
+
+            if(mysqli_errno($povezava) == 1062){
+                mysqli_close($povezava);
+                http_response_code(400);
+                echo json_encode(array("sporocilo" => "Vnos že obstaja"));
+                exit;
+            }
+
+            mysqli_close($povezava);
+            echo json_encode(array("sporocilo" => "Neka napaka se je zglodila pri izvajanju"));
+            http_response_code(500);
+            exit; 
+        }
+
+        
     }
     else{
+        //Če ni vpisana tabela vrne to
         mysqli_close($povezava);
         http_response_code(400);
         echo json_encode(array("sporocilo" => "Vse ni vključeno"));
@@ -113,6 +213,7 @@ if(mysqli_num_rows($rezultat) > 0){
     }
 }
 else{
+    //Če Token ne obstaja vrne, da nima dostopa
     mysqli_close($povezava);
     http_response_code(401);
     exit;
@@ -128,7 +229,10 @@ function BranjeStolpcev($tabela, $povezava){
         $podatki = array();
 
         while($vrstica = mysqli_fetch_assoc($rezultat)){
-            array_push($podatki, array($vrstica['Field'], $vrstica['Null']));
+            //Preveri, da nima atribut nastavljene možnosti za auto_increment
+            if($vrstica['Extra'] != "auto_increment"){
+                array_push($podatki, array($vrstica['Field'], $vrstica['Null']));
+            }
         }
         return $podatki;
     }
